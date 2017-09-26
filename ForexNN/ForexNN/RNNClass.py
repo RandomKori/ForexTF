b70fb8a2c1c292662a8883650932d9ceb5d919c7
RandomKori <randomkori40@gmail.com>
2017-09-27 00:12:30 +0600

    v0.1

--- ForexNN/ForexNN/RNNClass.py
+++ ForexNN/ForexNN/RNNClass.py
@@ -2,29 +2,36 @@ import Readers as rd
 import numpy as np
 import tensorflow as tf
 
-INITIAL_LEARNING_RATE = 0.5
+INITIAL_LEARNING_RATE = 0.1
 LEARNING_RATE_DECAY_STEPS = 15000
 LEARNING_RATE_DECAY_RATE = 0.96
 EPOCHS=100
-BATCH_SIZE=512
-LAYERS=5
+BATCH_SIZE=1024
+LAYERS=15
 
 def model_rnn(x_t,y_t,x_e,y_e):
     with tf.variable_scope("Inputs"):
-        x=tf.placeholder(tf.float32,[None,45,1],"Input")
+        x=tf.placeholder(tf.float32,[None,15,3],"Input")
         y=tf.placeholder(tf.float32,[None,3],"Output")
         training = tf.placeholder_with_default(input=False, shape=None, name="dropout_switch")
+        with tf.variable_scope("learning_rate"):
+            global_step = tf.Variable(initial_value=0, trainable=False, name="global_step")
+            l_rate = tf.train.exponential_decay(learning_rate=INITIAL_LEARNING_RATE, global_step=global_step,
+                                                    decay_steps=LEARNING_RATE_DECAY_STEPS,
+                                                    decay_rate=LEARNING_RATE_DECAY_RATE, staircase=True,
+                                                    name="learning_rate")
     with tf.variable_scope("Net"):
-        l_cells=[tf.nn.rnn_cell.BasicRNNCell(45) for _ in range(LAYERS)]       
-        cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)
-        rnn_output, rnn_state = tf.nn.dynamic_rnn(cell=cells, inputs=x, dtype=tf.float32)
+        l_cells=[tf.nn.rnn_cell.BasicRNNCell(6) for _ in range(LAYERS)]
+        rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)     
+        rnn_output, rnn_state = tf.nn.dynamic_rnn(cell=rnn_cells, inputs=x, dtype=tf.float32)
     with tf.variable_scope("predictions"):
         output = rnn_state[-1]
-        prediction = tf.layers.dense(inputs=output, units=3, name="prediction")
+        x1 = tf.layers.dropout(inputs=output, rate=0.5, training=training, name="dropout")
+        prediction = tf.layers.dense(inputs=x1, units=3, name="prediction")
     with tf.variable_scope("train"):
         global_step = tf.Variable(initial_value=0, trainable=False, name="global_step")
         loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=prediction)
-        train_step = tf.train.MomentumOptimizer(learning_rate=0.04,momentum=0.8,use_nesterov=True).minimize(loss=loss, global_step=global_step)
+        train_step = tf.train.MomentumOptimizer(learning_rate=l_rate,momentum=0.8,use_nesterov=True).minimize(loss=loss, global_step=global_step)
         tf.summary.scalar(name="Cross Entropy", tensor=loss)
 
     idx = list(range(x_t.shape[0]))
@@ -53,9 +60,9 @@ def model_rnn(x_t,y_t,x_e,y_e):
     return
 
 x_t,y_t=rd.ReadDataClass("./Data/train.csv")
-x_t.resize((x_t.shape[0],45,1))
+x_t.resize((x_t.shape[0],15,3))
 x_e,y_e=rd.ReadDataClass("./Data/test.csv")
-x_e.resize((x_e.shape[0],45,1))
+x_e.resize((x_e.shape[0],15,3))
 print("Тренировка модели")
 model_rnn(x_t,y_t,x_e,y_e)
 print("Тренировка закончена")
