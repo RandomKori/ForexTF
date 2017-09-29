a05faa025a71e1832acf5730309368a0cfa06a07
RandomKori <randomkori40@gmail.com>
2017-09-29 10:14:00 +0600

    v0.1

--- ForexNN/ForexNN/LTSMClass.py
+++ ForexNN/ForexNN/LTSMClass.py
@@ -4,13 +4,13 @@ import tensorflow as tf
 
 INITIAL_LEARNING_RATE = 0.03
 LEARNING_RATE_DECAY_RATE = 0.96
-EPOCHS=10000
+EPOCHS=100
 BATCH_SIZE=1024
 LAYERS=5
 
 def model_rnn(x_t,y_t,x_e,y_e):
     with tf.variable_scope("Inputs"):
-        x=tf.placeholder(tf.float32,[None,3,3],"Input")
+        x=tf.placeholder(tf.float32,[None,10,3],"Input")
         y=tf.placeholder(tf.float32,[None,3],"Output")
         training = tf.placeholder_with_default(input=False, shape=None, name="dropout_switch")
         with tf.variable_scope("learning_rate"):
@@ -24,14 +24,14 @@ def model_rnn(x_t,y_t,x_e,y_e):
 
     with tf.variable_scope("Net"):
         l_cells=[tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.sigmoid) for _ in range(3)]
-        rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)     
-        rnn_output, rnn_state = tf.nn.dynamic_rnn(cell=rnn_cells, inputs=x, dtype=tf.float32,scope="layer_inp")
+        rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)
+        output, state = tf.nn.dynamic_rnn(rnn_cells,x,dtype=tf.float32,scope="LTSM_l_inp")  
         for i in range(LAYERS):
             l_cells=[tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.sigmoid) for _ in range(3)]
-            rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)     
-            rnn_output, rnn_state = tf.nn.dynamic_rnn(cell=rnn_cells, inputs=rnn_output, dtype=tf.float32,scope="layer_"+"{}".format(i))
+            rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)
+            output, state = tf.nn.dynamic_rnn(rnn_cells,output,dtype=tf.float32,scope="LTSM_l_"+"{}".format(i))
     with tf.variable_scope("predictions"):
-        output = rnn_state[0][0]
+        output = state[0][0]
         prediction = tf.layers.dense(inputs=output, units=3, name="prediction")
 
     with tf.variable_scope("train"):
@@ -50,7 +50,7 @@ def model_rnn(x_t,y_t,x_e,y_e):
         test_writer = tf.summary.FileWriter(logdir="./logs/test/", graph=sess.graph)
         sess.run(fetches=init_global)
         for e in range(1, EPOCHS + 1):
-            np.random.shuffle(idx)
+            #np.random.shuffle(idx)
             batch_generator = (idx[i * BATCH_SIZE:(1 + i) * BATCH_SIZE] for i in range(n_batches))
             for s in range(n_batches):
                 id_batch = next(batch_generator)
@@ -71,9 +71,9 @@ def model_rnn(x_t,y_t,x_e,y_e):
     return
 
 x_t,y_t=rd.ReadDataClassLTSM("./Data/train.csv")
-x_t.resize((x_t.shape[0],3,3))
+x_t.resize((x_t.shape[0],10,3))
 x_e,y_e=rd.ReadDataClassLTSM("./Data/test.csv")
-x_e.resize((x_e.shape[0],3,3))
+x_e.resize((x_e.shape[0],10,3))
 print("Тренировка модели")
 model_rnn(x_t,y_t,x_e,y_e)
 print("Тренировка закончена")
