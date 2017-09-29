01fb41dfb22bb11754c2ca0e3c80251d7907fabc
RandomKori <randomkori40@gmail.com>
2017-09-29 10:20:58 +0600

    v0.1

--- ForexNN/ForexNN/Readers.py
+++ ForexNN/ForexNN/Readers.py
@@ -35,11 +35,13 @@ def ReadDataClassBULab(s):
 
 
 def ReadDataClassLTSM(s):
-    dt={'s1':np.float64,'s2':np.float64,'s3':np.float64,'s4':np.float64,'s5':np.float64,'s6':np.float64,'s7':np.float64,'s8':np.float64,'s9':np.float64,
+    dt={'s1':np.float64,'s2':np.float64,'s3':np.float64,'s4':np.float64,'s5':np.float64,'s6':np.float64,'s7':np.float64,'s8':np.float64,'s9':np.float64,'s10':np.float64,
+        's11':np.float64,'s12':np.float64,'s13':np.float64,'s14':np.float64,'s15':np.float64,'s16':np.float64,'s17':np.float64,'s18':np.float64,'s19':np.float64,'s20':np.float64,
+        's21':np.float64,'s22':np.float64,'s23':np.float64,'s24':np.float64,'s25':np.float64,'s26':np.float64,'s27':np.float64,'s28':np.float64,'s29':np.float64,'s30':np.float64,
         'l1':np.float64,'l2':np.float64,'l3':np.float64}
     dat=pd.read_csv(s,';',dtype=dt)
-    x=dat[dat.columns[:9]].values
-    y=dat[dat.columns[9:]].values
+    x=dat[dat.columns[:30]].values
+    y=dat[dat.columns[30:]].values
     x=np.array(x)
     y=np.array(y)
     return x,y
