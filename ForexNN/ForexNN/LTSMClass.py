73b9c85fef3377aadb1a14fe555704bd3628278b
RandomKori <randomkori40@gmail.com>
2017-09-29 11:53:11 +0600

    v0.1

--- ForexNN/ForexNN/LTSMClass.py
+++ ForexNN/ForexNN/LTSMClass.py
@@ -4,32 +4,24 @@ import tensorflow as tf
 
 INITIAL_LEARNING_RATE = 0.03
 LEARNING_RATE_DECAY_RATE = 0.96
-EPOCHS=1000
-BATCH_SIZE=1024
-LAYERS=5
+EPOCHS = 1000
+BATCH_SIZE = 1024
+LAYERS = 5
 
 def model_rnn(x_t,y_t,x_e,y_e):
     with tf.variable_scope("Inputs"):
-        x=tf.placeholder(tf.float32,[None,10,3],"Input")
-        y=tf.placeholder(tf.float32,[None,3],"Output")
-        training = tf.placeholder_with_default(input=False, shape=None, name="dropout_switch")
-        with tf.variable_scope("learning_rate"):
-            global_step = tf.Variable(initial_value=0, trainable=False, name="global_step")
-            l_rate = tf.train.exponential_decay(learning_rate=INITIAL_LEARNING_RATE, global_step=global_step,
-                                                    decay_steps=50*BATCH_SIZE,
-                                                    decay_rate=LEARNING_RATE_DECAY_RATE, staircase=True,
-                                                    name="learning_rate")
-            tf.summary.scalar(name="learning_rate", tensor=l_rate)
-            tf.summary.scalar(name="global_step", tensor=global_step)
+        x = tf.placeholder(tf.float32,[None,10,3],"Input")
+        y = tf.placeholder(tf.float32,[None,3],"Output")
 
     with tf.variable_scope("Net"):
-        l_cells=[tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.sigmoid) for _ in range(3)]
+        l_cells = [tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.relu) for _ in range(3)]
         rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)
         output, state = tf.nn.dynamic_rnn(rnn_cells,x,dtype=tf.float32,scope="LTSM_l_inp")  
         for i in range(LAYERS):
-            l_cells=[tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.sigmoid) for _ in range(3)]
+            l_cells = [tf.nn.rnn_cell.BasicLSTMCell(9,activation=tf.nn.relu) for _ in range(3)]
             rnn_cells = tf.nn.rnn_cell.MultiRNNCell(cells=l_cells)
-            output, state = tf.nn.dynamic_rnn(rnn_cells,output,dtype=tf.float32,scope="LTSM_l_"+"{}".format(i))
+            output, state = tf.nn.dynamic_rnn(rnn_cells,output,dtype=tf.float32,scope="LTSM_l_" + "{}".format(i))
+        
     with tf.variable_scope("predictions"):
         output = state[0][0]
         prediction = tf.layers.dense(inputs=output, units=3, name="prediction")
@@ -37,7 +29,7 @@ def model_rnn(x_t,y_t,x_e,y_e):
     with tf.variable_scope("train"):
         global_step = tf.Variable(initial_value=0, trainable=False, name="global_step")
         loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=y, logits=prediction)
-        train_step = tf.train.MomentumOptimizer(learning_rate=0.01, momentum=0.5, use_nesterov=True).minimize(loss=loss, global_step=tf.train.get_global_step())
+        train_step = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss=loss)
         tf.summary.scalar(name="Cross Entropy", tensor=loss)
 
     idx = list(range(x_t.shape[0]))
@@ -54,25 +46,25 @@ def model_rnn(x_t,y_t,x_e,y_e):
             batch_generator = (idx[i * BATCH_SIZE:(1 + i) * BATCH_SIZE] for i in range(n_batches))
             for s in range(n_batches):
                 id_batch = next(batch_generator)
-                feed = {x: x_t[id_batch], y: y_t[id_batch], training: True}
-                summary,acc= sess.run([merged, train_step], feed_dict=feed)
-                train_writer.add_summary(summary, e*n_batches+s)
-            summary,acc = sess.run([merged, loss],feed_dict={x: x_e, y: y_e, training: False})
+                feed = {x: x_t[id_batch], y: y_t[id_batch]}
+                summary,acc = sess.run([merged, train_step], feed_dict=feed)
+                train_writer.add_summary(summary, e * n_batches + s)
+            summary,acc = sess.run([merged, loss],feed_dict={x: x_e, y: y_e})
             test_writer.add_summary(summary, e)
-            loss_train = loss.eval(feed_dict={x: x_t, y: y_t, training: False})
-            loss_test = loss.eval(feed_dict={x: x_e, y: y_e, training: False})
+            loss_train = loss.eval(feed_dict={x: x_t, y: y_t})
+            loss_test = loss.eval(feed_dict={x: x_e, y: y_e})
             print("Эпоха: {0} Ошибка: {1} Ошибка на тестовых данных: {2}".format(e,loss_train,loss_test))
-            if(loss_train<0.02):
+            if(loss_train < 0.02):
                 break
         saver.save(sess=sess, save_path="./ModelRNNClass/RNNClass")
-        rez=sess.run(prediction,feed_dict={x: x_e, training: False})
+        rez = sess.run(prediction,feed_dict={x: x_e, training: False})
         for i in range(len(rez)):
             print(rez[i])
     return
 
-x_t,y_t=rd.ReadDataClassLTSM("./Data/train.csv")
+x_t,y_t = rd.ReadDataClassLTSM("./Data/train.csv")
 x_t.resize((x_t.shape[0],10,3))
-x_e,y_e=rd.ReadDataClassLTSM("./Data/test.csv")
+x_e,y_e = rd.ReadDataClassLTSM("./Data/test.csv")
 x_e.resize((x_e.shape[0],10,3))
 print("Тренировка модели")
 model_rnn(x_t,y_t,x_e,y_e)
